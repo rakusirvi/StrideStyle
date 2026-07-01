4449982fbb3655af5b81ff1e6e06bc944499c5a1
Rakesh Choudhary <rakeshchoudhary1154@gmail.com>
2026-07-01 22:02:18 +0530

    Project Initial Stage

--- src/Features/LandingPage/Sections/HeroSection.tsx
+++ src/Features/LandingPage/Sections/HeroSection.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const HeroSection = () => {
-  return <div>HeroSection</div>;
+  return <div className="w-full min-h-screen p-4 md:p-2"></div>;
 };
 
 export default HeroSection;
